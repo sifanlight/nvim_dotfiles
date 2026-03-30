0f6bd5766ebfc10ee634cf4444d9ee0eb0024135
Sina Karimi <si.karimi1378@gmail.com>
2026-03-30 13:51:05 -0400

    Fixed clang problem

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,14 +1,12 @@
 return {
   "neovim/nvim-lspconfig",
   opts = {
-    setup = {
-      clangd = function(_, opts)
-        opts.capabilities.offsetEncoding = { "utf-16" }
-      end,
-    },
     -- Ensure semantic tokens are enabled for that "VS Code" color depth
     servers = {
       clangd = {
+        capabilities = {
+          offsetEncoding = { "utf-16" },
+        },
         keys = {
           { "<leader>ch", "<cmd>ClangdSwitchSourceHeader<cr>", desc = "Switch Source/Header (C++)" },
         },
